15d2bbe189e79675c810067b0a352df64c32c8c1
Felipe Semedo <felipesemedo05@gmail.com>
2025-03-05 01:42:23 -0300

    PIVOTT

--- app.py
+++ app.py
@@ -26,23 +26,18 @@ st.title("Spotify API - Seleção de Usuário")
 # Seleção do usuário
 selected_user = st.selectbox("Selecione um usuário:", list(USERS.keys()))
 
-# Iniciar autenticação ao selecionar usuário
-user_data = USERS[selected_user]
-auth_manager = SpotifyOAuth(
-    client_id=user_data["CLIENT_ID"],
-    client_secret=user_data["CLIENT_SECRET"],
-    redirect_uri=user_data["REDIRECT_URI"],
-    scope=SCOPE,
-)
-auth_url = auth_manager.get_authorize_url()
-st.write("[Clique aqui para autenticar no Spotify](%s)" % auth_url)
-
-redirect_url = st.text_input("Após autenticar, cole o URL de redirecionamento aqui:")
-
-if redirect_url:
-    code = auth_manager.parse_response_code(redirect_url)
-    token_info = auth_manager.get_access_token(code)
-    sp = spotipy.Spotify(auth=token_info['access_token'])
+if st.button("Iniciar Sessão"):
+    user_data = USERS[selected_user]
+    auth_manager = SpotifyOAuth(
+        client_id=user_data["CLIENT_ID"],
+        client_secret=user_data["CLIENT_SECRET"],
+        redirect_uri=user_data["REDIRECT_URI"],
+        scope=SCOPE,
+    )
+    webbrowser.open(auth_manager.get_authorize_url())
+    
+    token_info = auth_manager.get_access_token(as_dict=False)
+    sp = spotipy.Spotify(auth=token_info)
     
     # Obter informações do usuário
     user_profile = sp.current_user()
